8ec1faec9cc00133d2d8eb8b569cff7ffccec9ce
tomegz <tomaszbubala@o2.pl>
2017-04-22 00:58:44 +0200

    add styles and js to play sounds and remove transitions

---
+++ js-drum-kit/script.js
@@ -0,0 +1,22 @@
+(function() {
+    //play sound every time key is down
+    window.addEventListener("keydown", playSound);
+    //remove transition every time it ends on any key
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+
+    function playSound(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //ES6 template string
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if(!audio) return; //only if audio exists
+        audio.currentTime = 0; //rewind before each play
+        audio.play();
+        key.classList.add('playing');
+    }  
+
+    function removeTransition(e) {
+        if(e.propertyName !== "transform") return; //because event has about 6 different transitionend events..
+        this.classList.remove('playing');
+    }
+    
+}());
