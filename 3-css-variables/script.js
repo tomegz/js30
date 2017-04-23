8586569a07d1c2c970c9582e772cdfb8d5ee35d0
tomegz <tomaszbubala@o2.pl>
2017-04-23 22:28:32 +0200

    finish project

---
+++ 3-css-variables/script.js
@@ -0,0 +1,7 @@
+const inputs = document.querySelectorAll("#controls input");
+inputs.forEach(input => input.addEventListener("input", handleUpdate));
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
