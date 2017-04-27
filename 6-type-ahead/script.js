13e818320187ff03f75ae5ee5757b5d5374e4b55
tomegz <tomaszbubala@o2.pl>
2017-04-27 13:06:19 +0200

    start 6th project - js typing ahead

---
+++ 6-type-ahead/script.js
@@ -0,0 +1,5 @@
+const source = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = [];
+fetch(source)
+    .then(response => response.json())
+    .then(data => cities.push(...data));
