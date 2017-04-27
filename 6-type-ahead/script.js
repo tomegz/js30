35861c252b4a8ceeefa201c3fcaf1f7c2128f9a9
tomegz <tomaszbubala@o2.pl>
2017-04-27 13:23:40 +0200

    finish filtering function

--- 6-type-ahead/script.js
+++ 6-type-ahead/script.js
@@ -2,4 +2,11 @@ const source = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29
 const cities = [];
 fetch(source)
     .then(response => response.json())
-    .then(data => cities.push(...data));
+    .then(data     => cities.push(...data));
+
+function findMatches(wordToMatch) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
