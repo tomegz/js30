c42a5da5af9a49ca2a680fe9f083dc8e4f520e3a
tomegz <tomaszbubala@o2.pl>
2017-04-27 21:20:32 +0200

    add highlight, population count is now shown with commas, refactor

--- 6-type-ahead/script.js
+++ 6-type-ahead/script.js
@@ -11,24 +11,26 @@ searchBar.addEventListener("input", showMatches)
 
 function showMatches() {
     const prefix = this.value;
-    const matches = findMatches(prefix);
-    appendSuggestions(matches);
+    const regex = new RegExp(prefix, 'gi');
+    const places = findMatches(regex);
+    const htmlToAppend = places.map(place => {
+        const cityName = place.city.replace(regex, `<span class="hl">${prefix}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${prefix}</span>`);
+        const population = numberWithCommas(place.population);
+        return `<li>
+                    <span class="name">${cityName}, ${stateName}</span>
+                    <span class="population">${population}</span>
+                </li>`;
+    }).join("");
+    suggestions.innerHTML = htmlToAppend;
 }
 
-function findMatches(wordToMatch) {
+function findMatches(regex) {
     return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex);
     });
 }
 
-function appendSuggestions(places) {
-    let html = "";
-    places.forEach(place => {
-        html += `<li>
-                     <span class="name">${place.city}, ${place.state}</span>
-                     <span class="population">${place.population}</span>
-                 </li>`;
-    });
-    suggestions.innerHTML = html;
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
