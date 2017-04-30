8bb3d51a7a4f20a08e9bd0b8abc1efb8273f6e8d
tomegz <tomaszbubala@o2.pl>
2017-04-30 20:29:54 +0200

    start 11th project

---
+++ 11-custom-video-player/script.js
@@ -0,0 +1,18 @@
+/* Get elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Listeners
+
+video.addEventListener("click", togglePlay);
+
+//Functions
+
+function togglePlay() {
+    video.paused ? video.play() : video.pause();
+}
