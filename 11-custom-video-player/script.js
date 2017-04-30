f6bcd27feecc8cd20c2b738c9b6d8dbffa064d04
tomegz <tomaszbubala@o2.pl>
2017-04-30 21:25:01 +0200

    finish player functionality

--- 11-custom-video-player/script.js
+++ 11-custom-video-player/script.js
@@ -6,13 +6,49 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+let mousedown = false;
 
 // Listeners
 
 video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("input", handleRangeUpdate));
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
 
 //Functions
 
 function togglePlay() {
     video.paused ? video.play() : video.pause();
+    updateButton();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    const timeToSkip = parseFloat(this.dataset.skip);
+    console.log(timeToSkip);
+    video.currentTime += timeToSkip;
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    video.currentTime = video.duration * (e.offsetX / progress.offsetWidth);
 }
