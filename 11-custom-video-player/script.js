f2b0be793ff6d43b45845ca97cdbacbd00516ee2
tomegz <tomaszbubala@o2.pl>
2017-04-30 21:59:24 +0200

    add fullscreen button

--- 11-custom-video-player/script.js
+++ 11-custom-video-player/script.js
@@ -6,8 +6,8 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreenButton = player.querySelector('.full_screen_button');
 let mousedown = false;
-
 // Listeners
 
 video.addEventListener("click", togglePlay);
@@ -21,6 +21,7 @@ progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => mousedown = true);
 progress.addEventListener("mouseup", () => mousedown = false);
+fullScreenButton.addEventListener("click", goFullScreen);
 
 //Functions
 
@@ -34,6 +35,10 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function updateFullScreenButton() {
+    const icon = fullScreenButton.textContent === "[ ]" ? "--" : "[ ]";
+    fullScreenButton.textContent = icon;
+}
 function skip() {
     const timeToSkip = parseFloat(this.dataset.skip);
     console.log(timeToSkip);
@@ -51,4 +56,8 @@ function handleProgress() {
 
 function scrub(e) {
     video.currentTime = video.duration * (e.offsetX / progress.offsetWidth);
+}
+
+function goFullScreen() {
+    player.webkitRequestFullscreen();
 }
