be291d879a23e8d2cfd1519963cdca94dac12053
tomegz <tomaszbubala@o2.pl>
2017-05-05 22:54:29 +0200

    finish 13th project

--- 13-slide-in-on-scroll/script.js
+++ 13-slide-in-on-scroll/script.js
@@ -1,3 +1,22 @@
+const sliderImages = document.querySelectorAll(".slide-in");
+
+document.addEventListener("scroll", debounce(checkSlide));
+
+function checkSlide(e) {
+    sliderImages.forEach(sliderImage => {
+        //this is being recalculated every time, in case anything was changed on the page
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2; // half of image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height; // bottom of image
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add("active");
+        } else {
+            sliderImage.classList.remove("active");
+        }
+    })
+}
+
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
@@ -11,12 +30,4 @@ function debounce(func, wait = 20, immediate = true) {
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
     };
-}
-
-const sliderImages = document.querySelectorAll(".slide-in");
-
-document.addEventListener("scroll", checkSlide);
-
-function checkSlide(e) {
-    console.log(e);
 }
