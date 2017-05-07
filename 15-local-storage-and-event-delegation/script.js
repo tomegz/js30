535148685edbf2b616fef039e260446b0ce6e90a
tomegz <tomaszbubala@o2.pl>
2017-05-07 22:22:39 +0200

    add local storage saving

--- 15-local-storage-and-event-delegation/script.js
+++ 15-local-storage-and-event-delegation/script.js
@@ -1,3 +1,30 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+addItems.addEventListener("submit", addItem);
+populateList(items, itemsList);
+
+function addItem(e) {
+    const text = (this.querySelector("[name=item]")).value;
+    const item = {
+        text,
+        done: false
+    };
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem("items", JSON.stringify(items));
+    this.reset();
+    e.preventDefault();
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+       return `
+         <li>
+           <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? "checked" : ""}/>
+           <label for="item${i}">${plate.text}</label>
+         </li> 
+       `; 
+    }).join("");
+}
