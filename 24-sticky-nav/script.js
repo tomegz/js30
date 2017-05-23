35c410d235f350871040bcf78c7b4f94fe12418b
tomegz <tomaszbubala@o2.pl>
2017-05-24 00:11:15 +0200

    finish 24th project

--- 24-sticky-nav/script.js
+++ 24-sticky-nav/script.js
@@ -1,6 +1,12 @@
 const nav = document.querySelector("#main");
-
+const topOfNav = nav.offsetTop;
 window.addEventListener("scroll", fixNav);
 function fixNav() {
-    
+    if(window.scrollY >= topOfNav) {
+      document.body.style.paddingTop = `${nav.offsetHeight}px`;
+      document.body.classList.add("fixed-nav");
+    } else {
+      document.body.style.paddingTop = 0;
+      document.body.classList.remove("fixed-nav");
+    }
 }
