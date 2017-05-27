6803ef9fbdf6ac3a3b449b40ab5dc77e2b9c0e0f
tomegz <tomaszbubala@o2.pl>
2017-05-27 19:03:35 +0200

    finish 30th project

--- 30-whack-a-mole/script.js
+++ 30-whack-a-mole/script.js
@@ -1,3 +1,45 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+moles.forEach(mole => mole.addEventListener("click", bonk));
+
+function randTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if(hole === lastHole) return randomHole(holes);
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add("up");
+    setTimeout(() => {
+        hole.classList.remove("up");
+        if(!timeUp) peep();
+    }, time);
+}
+
+function startGame() {
+    score = 0;
+    scoreBoard.textContent = score;
+    timeUp = false;
+    setTimeout(() => timeUp = true, 10000);
+    peep();
+}
+
+function bonk(e) {
+    if(!e.isTrusted) return; //no cheating
+    score++;
+    this.classList.remove("up");
+    scoreBoard.textContent = score;
+}
